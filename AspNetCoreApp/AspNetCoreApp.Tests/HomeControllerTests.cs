0657f16015b500f28ef979a0d406008796b790ab
Todor Velichkov Dimitrov <toshkooooooooooo@gmail.com>
2018-06-02 12:10:38 +0300

    Adding Unit Test project in the solution

---
+++ AspNetCoreApp/AspNetCoreApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AspNetCoreApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
